c509584d9c46cf903b320b6dca5399ac445e212a
Italo Spina <italo.spina@hotmail.com>
2025-02-14 10:44:22 -0300

    Terceiro commit

---
+++ tipos-variaveis/src/TiposDasVariaveis.java
@@ -0,0 +1,21 @@
+public class TiposDasVariaveis {
+    final double VALOR_DE_PI = 3.14;
+
+    /*
+     - Tipos Primitivos:
+        int → Números inteiros.
+        double → Números decimais.
+        boolean → Verdadeiro (true) ou falso (false).
+        char → Um único caractere.
+        
+     - Tipos Referência (Objetos):
+        String → Texto (ex: "Olá, Java!").
+        Arrays → Coleção de elementos do mesmo tipo (ex: int[] numeros = {1, 2, 3};).
+        
+     - Escopo de Variáveis:
+        Variáveis de Instância → Pertencem a um objeto (this.nome).
+        Variáveis Locais → Criadas dentro de um método.
+        Variáveis Estáticas (de Classe) → Usam static e pertencem à classe (static int contador).
+        Parâmetros de Método → Passados como argumento (void metodo(String texto)).
+     */
+}
