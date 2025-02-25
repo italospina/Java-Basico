8f55303360ac2c90e3160ed4716b8283fdb48299
Italo Spina <italo.spina@hotmail.com>
2025-02-25 19:25:44 -0300

    atualização formatação

--- 6-tratamento-excecao/src/FormatadorCepExemplo.java
+++ 6-tratamento-excecao/src/FormatadorCepExemplo.java
@@ -1,21 +1,28 @@
+// Classe de exceção personalizada
+class CepInvalidoException extends Exception {
+    public CepInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+}
+
 public class FormatadorCepExemplo {
     public static void main(String[] args) {
-        String cep = "1234567";
+        String cep = "1234567"; // Teste com um CEP inválido
         
         try {
             String cepFormatado = formatarCep(cep);
-            System.out.println(cepFormatado);
+            System.out.println("CEP formatado: " + cepFormatado);
         } catch (CepInvalidoException e) {
-            System.out.println("O CEP não possui 8 dígitos. " + e);		
+            System.out.println("Erro: " + e.getMessage());
         }
     }
 
     static String formatarCep(String cep) throws CepInvalidoException {
-        
-        if (cep.length() != 8) {
-            throw new CepInvalidoException();
+        if (cep.length() != 8 || !cep.matches("\\d{8}")) {
+            throw new CepInvalidoException("O CEP deve conter exatamente 8 dígitos numéricos.");
         }
-        
-        return "12.345-678";
+
+        // Formata o CEP no padrão XX.XXX-XXX
+        return cep.substring(0, 2) + "." + cep.substring(2, 5) + "-" + cep.substring(5, 8);
     }
 }
